705602493bdddd66dfb8f78dca7bac56ee64d45d
Thorsten de Vries <tedevs@gmail.com>
2020-06-17 08:24:13 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Thorsten de Vries on 17.06.20.
+//  Copyright © 2020 Thorsten de Vries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
